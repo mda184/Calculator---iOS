cb24a4991560e1231fcd0e4e3e16c40779410f66
Maria Aloysius <mda184@mun.ca>
2018-09-25 12:19:22 -0230

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Maria Aloysius on 2018-09-25.
+//  Copyright © 2018 Maria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by Maria Aloysius on 2018-09-25.
+//  Copyright © 2018 Maria. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
