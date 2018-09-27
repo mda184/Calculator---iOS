00434b224b03997ae913c0be7ae0ff819113c03e
Maria Aloysius <mda184@mun.ca>
2018-09-27 15:28:27 -0230

    Updated on 26 sept

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -9,14 +9,23 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)numberPressed:(UIButton *)sender;
+- (IBAction)operatorPressed:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *solutionLabel;
 
 @end
 
 @implementation ViewController
 
+bool isOperatorClicked = FALSE;
+char anyOperator;
+NSMutableString *firstDigit;
+NSMutableString *secondDigit;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    firstDigit = NULL;
+    secondDigit = NULL;
 }
 
 
@@ -26,4 +35,46 @@
 }
 
 
+- (IBAction)numberPressed:(UIButton *)sender {
+    while (!isOperatorClicked) {
+        
+        if ([sender.titleLabel.text isEqualToString:@"1"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"2"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"3"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"4"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"5"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"6"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"7"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"8"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"9"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+        else if ([sender.titleLabel.text isEqualToString:@"0"]){
+            [firstDigit stringByAppendingString:firstDigit];
+        }
+    }
+    NSLog(@"%@",firstDigit);
+    self.solutionLabel.text = [NSString stringWithFormat:@"%@",firstDigit];
+    
+    
+}
+
+- (IBAction)operatorPressed:(UIButton *)sender {
+}
 @end
